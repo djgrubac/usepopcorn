a89ba233ad8c33a1214f956d937fe6cf161fc8d3
djgrubac <grubacdjordje@gmail.com>
2023-10-04 15:40:22 +0200

    Fixed star rating

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 import StarRating from './StarRating';
 
+const Test = () => {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color='blue' maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie is rated {movieRating} stars</p>
+    </div>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']} defaultRating={1} />
+    <Test />
   </React.StrictMode>
 );
 
